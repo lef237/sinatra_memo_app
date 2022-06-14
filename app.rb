ebc07dc63b52d1462c7dfea3375fbfd76fb59ace
lef237 <lef237k5@gmail.com>
2022-06-14 16:35:45 +0900

    sinatraで色々設定を変えてみるテスト01

--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ require 'sinatra/reloader'
 get "/" do
   erb :index
 end
+
+get '/hello' do
+  content_type :json
+  data= {
+      message: "Hello"
+  }
+  data.to_json
+end
+
+get '/erb_template_page' do
+  erb :erb_template_page
+end
