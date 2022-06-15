104db2e2413048ab9e6df47a018a77d492428ad1
lef237 <lef237k5@gmail.com>
2022-06-15 18:45:30 +0900

    /newの画面でタイトルと本文を送信するとPOSTが発動してJSONが更新されるようにした

--- app.rb
+++ app.rb
@@ -10,9 +10,20 @@ get "/" do
   erb :index
 end
 
+post "/" do
+  # paramsをjsonに追加する処理を書く
+  memos = File.open("data.json") { |f| JSON.load(f) }
+  memo_id = memos.size + 1
+  memo_title = params[:memo_title]
+  memo_content = params[:memo_content]
+  hash = {"memo_id" =>memo_id, "memo_title" =>memo_title, "memo_content" =>memo_content}
+  memos << hash
+  open("data.json", "w") do |file|
+    JSON.dump(memos, file)
+  end
+end
+
 get "/new" do
-  @title = "sinatra_memo_app"
-  @content = "メモアプリ"
   erb :new
 end
 
