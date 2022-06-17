91ff1f4e6e32ccba6defb1ecbf599c100ecface3
lef237 <lef237k5@gmail.com>
2022-06-18 01:37:28 +0900

    Sinatraの動作確認完了。IDはJSONに書き込みではなく、配列の位置を元に算出することでバグを無くした

--- app.rb
+++ app.rb
@@ -6,17 +6,14 @@ get "/" do
   File.open("data.json") do |f|
     @memos = JSON.load(f)
   end
-  @memo_number = @memos.size - 1
   erb :index
 end
 
 post "/" do
-  # paramsをjsonに追加する処理を書く
   memos = File.open("data.json") { |f| JSON.load(f) }
-  memo_id = memos.size + 1
   memo_title = params[:memo_title]
   memo_content = params[:memo_content]
-  hash = {"memo_id" =>memo_id, "memo_title" =>memo_title, "memo_content" =>memo_content}
+  hash = {"memo_title" =>memo_title, "memo_content" =>memo_content}
   memos << hash
   open("data.json", "w") do |file|
     JSON.dump(memos, file)
@@ -46,16 +43,8 @@ delete "/show/:id" do
   redirect to('/')
 end
 
-# delete '/show/:id' do
-#   memos = File.open("data.json") { |f| JSON.load(f) }
-#   memos.delete(:id)
-#   open("data.json", "w") do |file|
-#     JSON.dump(memos, file)
-#   end
-#   redirect to('/')
-# end
-
 get "/edit/:id" do
+  @id = params[:id]
   File.open("data.json") do |f|
     @memos = JSON.load(f)
   end
@@ -75,12 +64,3 @@ patch "/" do
   end
   redirect to('/')
 end
-
-
-# post "/edit/:id" do
-#   File.open("data.json") do |f|
-#     @memos = JSON.load(f)
-#   end
-#   @memo = @memos[params[:id].to_i]
-#   erb :edit
-# end
