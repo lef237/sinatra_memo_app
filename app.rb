58cc95629c4e28b2fbb30dd37e8c4d548eee3b52
lef237 <lef237k5@gmail.com>
2022-06-18 02:25:02 +0900

    JSON.load(f)をJSON.parse(f.read)に変更した

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
 get '/' do
   File.open('data.json') do |f|
-    @memos = JSON.load(f)
+    @memos = JSON.parse(f.read)
   end
   erb :index
 end
 
 post '/' do
-  memos = File.open('data.json') { |f| JSON.load(f) }
+  memos = File.open('data.json') { |f| JSON.parse(f.read) }
   memo_title = params[:memo_title]
   memo_content = params[:memo_content]
   hash = { 'memo_title' => memo_title, 'memo_content' => memo_content }
@@ -28,14 +30,14 @@ end
 get '/show/:id' do
   @id = params[:id].to_i
   File.open('data.json') do |f|
-    @memos = JSON.load(f)
+    @memos = JSON.parse(f.read)
   end
   @memo = @memos[params[:id].to_i]
   erb :show
 end
 
 delete '/show/:id' do
-  memos = File.open('data.json') { |f| JSON.load(f) }
+  memos = File.open('data.json') { |f| JSON.parse(f.read) }
   memos.delete_at(params['id'].to_i)
   open('data.json', 'w') do |file|
     JSON.dump(memos, file)
@@ -46,14 +48,14 @@ end
 get '/edit/:id' do
   @id = params[:id]
   File.open('data.json') do |f|
-    @memos = JSON.load(f)
+    @memos = JSON.parse(f.read)
   end
   @memo = @memos[params['id'].to_i]
   erb :edit
 end
 
 patch '/' do
-  memos = File.open('data.json') { |f| JSON.load(f) }
+  memos = File.open('data.json') { |f| JSON.parse(f.read) }
   memo_id = params[:memo_id].to_i
   memo_title = params[:memo_title]
   memo_content = params[:memo_content]
