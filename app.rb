dc4a1207a8eed857a321df2fc40546cf1bb9833e
lef237 <lef237k5@gmail.com>
2022-06-25 16:36:36 +0900

    Karnel.#openをFile.openにした。'data.json'を定数に置き換えた

--- app.rb
+++ app.rb
@@ -4,20 +4,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
+DATA_JSON = 'data.json'
+
 get '/' do
-  File.open('data.json') do |f|
-    @memos = JSON.parse(f.read)
-  end
+  @memos = File.open(DATA_JSON) { |f| JSON.parse(f.read) }
   erb :index
 end
 
 post '/' do
-  memos = File.open('data.json') { |f| JSON.parse(f.read) }
+  memos = File.open(DATA_JSON) { |f| JSON.parse(f.read) }
   memo_title = params[:memo_title]
   memo_content = params[:memo_content]
   hash = { 'memo_title' => memo_title, 'memo_content' => memo_content }
   memos << hash
-  open('data.json', 'w') do |file|
+  File.open(DATA_JSON, 'w') do |file|
     JSON.dump(memos, file)
   end
   redirect to('/')
@@ -29,7 +29,7 @@ end
 
 get '/show/:id' do
   @id = params[:id].to_i
-  File.open('data.json') do |f|
+  File.open(DATA_JSON) do |f|
     @memos = JSON.parse(f.read)
   end
   @memo = @memos[params[:id].to_i]
@@ -37,9 +37,9 @@ get '/show/:id' do
 end
 
 delete '/show/:id' do
-  memos = File.open('data.json') { |f| JSON.parse(f.read) }
+  memos = File.open(DATA_JSON) { |f| JSON.parse(f.read) }
   memos.delete_at(params['id'].to_i)
-  open('data.json', 'w') do |file|
+  File.open(DATA_JSON, 'w') do |file|
     JSON.dump(memos, file)
   end
   redirect to('/')
@@ -47,7 +47,7 @@ end
 
 get '/edit/:id' do
   @id = params[:id]
-  File.open('data.json') do |f|
+  File.open(DATA_JSON) do |f|
     @memos = JSON.parse(f.read)
   end
   @memo = @memos[params['id'].to_i]
@@ -55,13 +55,13 @@ get '/edit/:id' do
 end
 
 patch '/' do
-  memos = File.open('data.json') { |f| JSON.parse(f.read) }
+  memos = File.open(DATA_JSON) { |f| JSON.parse(f.read) }
   memo_id = params[:memo_id].to_i
   memo_title = params[:memo_title]
   memo_content = params[:memo_content]
   memos[memo_id] = { 'memo_id' => memo_id, 'memo_title' => memo_title, 'memo_content' => memo_content }
 
-  open('data.json', 'w') do |file|
+  File.open(DATA_JSON, 'w') do |file|
     JSON.dump(memos, file)
   end
   redirect to('/')
