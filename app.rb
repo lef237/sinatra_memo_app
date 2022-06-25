2ca296081d51685c1f7fab27ae74d246b0522fe1
lef237 <lef237k5@gmail.com>
2022-06-25 18:22:58 +0900

    idをやめてmemo_idに統一した

--- app.rb
+++ app.rb
@@ -27,30 +27,30 @@ get '/new' do
   erb :new
 end
 
-get '/show/:id' do
-  @id = params[:id].to_i
+get '/show/:memo_id' do
+  @memo_id = params[:memo_id].to_i
   File.open(DATA_JSON) do |f|
     @memos = JSON.parse(f.read)
   end
-  @memo = @memos[params[:id].to_i]
+  @memo = @memos[params[:memo_id].to_i]
   erb :show
 end
 
-delete '/show/:id' do
+delete '/show/:memo_id' do
   memos = File.open(DATA_JSON) { |f| JSON.parse(f.read) }
-  memos.delete_at(params['id'].to_i)
+  memos.delete_at(params['memo_id'].to_i)
   File.open(DATA_JSON, 'w') do |file|
     JSON.dump(memos, file)
   end
   redirect to('/')
 end
 
-get '/edit/:id' do
-  @id = params[:id]
+get '/edit/:memo_id' do
+  @memo_id = params[:memo_id]
   File.open(DATA_JSON) do |f|
     @memos = JSON.parse(f.read)
   end
-  @memo = @memos[params['id'].to_i]
+  @memo = @memos[params['memo_id'].to_i]
   erb :edit
 end
 
