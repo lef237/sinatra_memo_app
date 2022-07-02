9dad17cac218de4c48de23e01a5bb2b04c78a50b
lef237 <lef237k5@gmail.com>
2022-07-02 13:23:04 +0900

    findメソッドを使った。三項演算子を使った

--- app.rb
+++ app.rb
@@ -32,8 +32,9 @@ end
 post '/memos' do
   loaded_json = read_json
   memo_id = loaded_json['id_counter'] + 1
-  memo_title = h(params['memo_title'])
-  memo_title = 'タイトル未設定' if memo_title == ''
+
+  h(params['memo_title']) == '' ? memo_title = 'タイトル未設定' : memo_title = h(params['memo_title'])
+
   memo_content = h(params['memo_content'])
   loaded_json['memos'] << { 'memo_id' => memo_id, 'memo_title' => memo_title, 'memo_content' => memo_content }
   loaded_json['id_counter'] = memo_id
@@ -47,8 +48,8 @@ end
 
 get '/memos/:memo_id' do
   memos = read_json['memos']
-  memos.each do |memo|
-    @memo = memo if memo['memo_id'] == params['memo_id'].to_i
+  @memo = memos.find do |memo|
+    memo['memo_id'] == params['memo_id'].to_i
   end
   erb :show
 end
@@ -65,8 +66,8 @@ end
 
 get '/memos/:memo_id/edit' do
   memos = read_json['memos']
-  memos.each do |memo|
-    @memo = memo if memo['memo_id'] == params['memo_id'].to_i
+  @memo = memos.find do |memo|
+    memo['memo_id'] == params['memo_id'].to_i
   end
   erb :edit
 end
@@ -74,8 +75,9 @@ end
 patch '/memos/:memo_id' do
   loaded_json = read_json
   memo_id = params['memo_id'].to_i
-  memo_title = h(params['memo_title'])
-  memo_title = 'タイトル未設定' if memo_title == ''
+  
+  h(params['memo_title']) == '' ? memo_title = 'タイトル未設定' : memo_title = h(params['memo_title'])
+
   memo_content = h(params['memo_content'])
   loaded_json['memos'].each do |memo|
     if memo['memo_id'] == memo_id
